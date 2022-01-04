01a57affc91c8d223d238a70897a95f18df6dfd3
Pratiti28 <79000977+Pratiti28@users.noreply.github.com>
2022-01-04 19:03:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 status="";
-
+objects=[];
 
 function setup(){
     canvas= createCanvas(480,380);
@@ -22,4 +22,38 @@ function modalloaded(){
 
 function draw(){
     image(video,0,0,480,380);
+    if(status!=""){
+    objectdetector.detect(video,gotresults);
+    for(i=0;i<objects.length;i++){
+        document.getElementById("status").innerHTML="status:objects detected";
+        fill("red");
+        percent=floor(objects[i].confidence*100);
+        text(objects[i].label+" "+percent+"%",objects[i].x,objects[i].y);
+        nofill();
+        stroke("red");
+        rect(objects[i].x,objects[i].y,object[i].width,objects[i].height);
+        if(objects[i].label==objectname){
+            video.stop()
+            objectdetector.detect(gotresults);
+            document.getElementById("object_status").innerHTML=objectname+" found";
+            synth=window.speechSynthesis;
+            utterthis=new SpeechSynthesisUtterance(objectname+" found");
+            synth.speak(utterthis);
+
+        }
+        else{
+            document.getElementById("object_status").innerHTML=objectname+" not found";
+        }
+    }
+    }
+}
+
+function gotresults(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        objects=results;
+    }
 }
